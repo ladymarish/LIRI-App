537f5d563d4975af8d9ad2573ce16899d5ee9a1b
Marina-Saladukha <marina@reunionmarketing.com>
2018-03-30 02:02:47 -0400

    finished

--- liri.js
+++ liri.js
@@ -26,32 +26,65 @@ if (process.argv[2] == "twitter-feed") {
 
 
 //spotify-this-song
-fs.readFile("random.txt", "utf8", function(err, data) {
-  if (err) {
-      return console.log(err);
-  }
-  var random = data.split(",");
-
-  if (process.argv[2] == random[0]) {
-	spotify.search({ type: 'track', query: random[1]}, function(err, data) {
+if (process.argv[2] == "spotify-this-song" && process.argv[3] !== undefined) {
+	spotify.search({ type: 'track', query: process.argv[3]}, function(err, data) {
 	    if (err) {
           console.log(err);
         }
         var mySpotify = ("\nArtist: " + data.tracks.items[0].album.artists[0].name + "\nThe Song's Name: " + data.tracks.items[0].name + "\nThe Preview Link: " + data.tracks.items[0].preview_url + "\nThe Album this song is from: " + data.tracks.items[0].album.name + "\n");
 		console.log(mySpotify); 	  
-  });
+    });
+   }
+   else if (process.argv[2] == "spotify-this-song" && process.argv[3] == undefined) {
+	spotify.search({ type: 'track', query: 'soy+yo'}, function(err, data) {
+	    if (err) {
+          console.log(err);
+        }
+        var mySpotify = ("\nArtist: " + data.tracks.items[0].album.artists[0].name + "\nThe Song's Name: " + data.tracks.items[0].name + "\nThe Preview Link: " + data.tracks.items[0].preview_url + "\nThe Album this song is from: " + data.tracks.items[0].album.name + "\n");
+		console.log(mySpotify); 	  
+    });
  }
-});
 
 //movie-this
 if (process.argv[2] == "movie-this") {
     var movieName = process.argv[3];
-	var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+    if (movieName !== undefined) {
+	    var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+        request(queryUrl, function(error, response, body) {
+            if (!error && response.statusCode === 200) {
+            var myMovie = ("\nMovie Title: " + JSON.parse(body).Title + "\nRelease Year: " + JSON.parse(body).Year + "\nMovie Rating: " + JSON.parse(body).Rated + "\nRotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value + "\nCountry: " + JSON.parse(body).Country + "\nLanguage: " + JSON.parse(body).Language + "\n");
+		    console.log(myMovie); 	  
+         };
+      });
+    }
+    else {
+    	var queryUrl = "http://www.omdbapi.com/?t=Mr.Nobody&y=&plot=short&apikey=trilogy";
+        request(queryUrl, function(error, response, body) {
+            if (!error && response.statusCode === 200) {
+            var myMovie = ("\nMovie Title: " + JSON.parse(body).Title + "\nRelease Year: " + JSON.parse(body).Year + "\nMovie Rating: " + JSON.parse(body).Rated + "\nRotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value + "\nCountry: " + JSON.parse(body).Country + "\nLanguage: " + JSON.parse(body).Language + "\n");
+		    console.log(myMovie); 	  
+          };
+      });
+    }
+}
 
-    request(queryUrl, function(error, response, body) {
-        if (!error && response.statusCode === 200) {
-          var myMovie = ("\nMovie Title: " + JSON.parse(body).Title + "\nRelease Year: " + JSON.parse(body).Year + "\nMovie Rating: " + JSON.parse(body).Rated + "\nRotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value + "\nCountry: " + JSON.parse(body).Country + "\nLanguage: " + JSON.parse(body).Language + "\n");
-		  console.log(myMovie); 	  
+
+
+//do-what-it-says
+if (process.argv[2] == "do-what-it-says") {
+
+  fs.readFile("random.txt", "utf8", function(err, data) {
+    if (err) {
+      return console.log(err);
+    }
+    var random = data.split(",");
+
+	spotify.search({ type: 'track', query: random[1]}, function(err, data) {
+	    if (err) {
+          console.log(err);
         }
+        var mySpotify = ("\nArtist: " + data.tracks.items[0].album.artists[0].name + "\nThe Song's Name: " + data.tracks.items[0].name + "\nThe Preview Link: " + data.tracks.items[0].preview_url + "\nThe Album this song is from: " + data.tracks.items[0].album.name + "\n");
+		console.log(mySpotify); 
+     });
     });
 }
