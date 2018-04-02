5290e80a9e19c1d9bf9bdcb5b0b0aa6ebaa94d09
Marina-Saladukha <marina@reunionmarketing.com>
2018-04-01 22:50:40 -0400

    bonus

--- liri.js
+++ liri.js
@@ -12,60 +12,74 @@ var client = new Twitter(keys.twitter);
 
 
 //my-tweets
-if (process.argv[2] == "twitter-feed") {
+if (process.argv[2] == "my-tweets") {
 	client.get('statuses/user_timeline', {q: 'node.js', count: 20, screen_name: 'Eat_Pray_Code'}, function(error, tweets, response) {
 	  if (error) {
         console.log(error);
       } 
 	   for (var i = 0; i < tweets.length; i++) {
          var myTweets = ("Date: " + tweets[i].created_at + "Tweet: " + tweets[i].text + "\n");
-		   console.log(myTweets); 	
-	  }
+		     console.log(myTweets); 	
+         fs.appendFile("log.txt", myTweets + "\n", function(err, data) {
+           if (err) throw err;
+         });
+	    }
   });
 }
 
-
 //spotify-this-song
 if (process.argv[2] == "spotify-this-song" && process.argv[3] !== undefined) {
-	spotify.search({ type: 'track', query: process.argv[3]}, function(err, data) {
-	    if (err) {
+  spotify.search({ type: 'track', query: process.argv[3]}, function(err, data) {
+      if (err) {
           console.log(err);
         }
         var mySpotify = ("\nArtist: " + data.tracks.items[0].album.artists[0].name + "\nThe Song's Name: " + data.tracks.items[0].name + "\nThe Preview Link: " + data.tracks.items[0].preview_url + "\nThe Album this song is from: " + data.tracks.items[0].album.name + "\n");
-		console.log(mySpotify); 	  
+    console.log(mySpotify);  
+    fs.appendFile("log.txt", mySpotify, function(err, data) {
+       if (err) throw err;
+    });   
     });
    }
    else if (process.argv[2] == "spotify-this-song" && process.argv[3] == undefined) {
-	spotify.search({ type: 'track', query: 'soy+yo'}, function(err, data) {
-	    if (err) {
+   spotify.search({ type: 'track', query: 'porz+goret'}, function(err, data) {
+      if (err) {
           console.log(err);
         }
         var mySpotify = ("\nArtist: " + data.tracks.items[0].album.artists[0].name + "\nThe Song's Name: " + data.tracks.items[0].name + "\nThe Preview Link: " + data.tracks.items[0].preview_url + "\nThe Album this song is from: " + data.tracks.items[0].album.name + "\n");
-		console.log(mySpotify); 	  
-    });
- }
+    console.log(mySpotify);  
+    fs.appendFile("log.txt", mySpotify, function(err, data) {
+       if (err) throw err;
+    });   
+  });
+}
 
 //movie-this
 if (process.argv[2] == "movie-this") {
     var movieName = process.argv[3];
     if (movieName !== undefined) {
-	    var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+      var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
         request(queryUrl, function(error, response, body) {
             if (!error && response.statusCode === 200) {
             var myMovie = ("\nMovie Title: " + JSON.parse(body).Title + "\nRelease Year: " + JSON.parse(body).Year + "\nMovie Rating: " + JSON.parse(body).Rated + "\nRotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value + "\nCountry: " + JSON.parse(body).Country + "\nLanguage: " + JSON.parse(body).Language + "\n");
-		    console.log(myMovie); 	  
+        console.log(myMovie);  
+        fs.appendFile("log.txt", myMovie, function(err, data) {
+           if (err) throw err;
+         });    
          };
       });
     }
     else {
-    	var queryUrl = "http://www.omdbapi.com/?t=Mr.Nobody&y=&plot=short&apikey=trilogy";
+      var queryUrl = "http://www.omdbapi.com/?t=Mr.Nobody&y=&plot=short&apikey=trilogy";
         request(queryUrl, function(error, response, body) {
             if (!error && response.statusCode === 200) {
             var myMovie = ("\nMovie Title: " + JSON.parse(body).Title + "\nRelease Year: " + JSON.parse(body).Year + "\nMovie Rating: " + JSON.parse(body).Rated + "\nRotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value + "\nCountry: " + JSON.parse(body).Country + "\nLanguage: " + JSON.parse(body).Language + "\n");
-		    console.log(myMovie); 	  
-          };
-      });
-    }
+        console.log(myMovie); 
+        fs.appendFile("log.txt", myMovie, function(err, data) {
+           if (err) throw err;
+         });     
+       };
+    });
+  }
 }
 
 
@@ -79,12 +93,15 @@ if (process.argv[2] == "do-what-it-says") {
     }
     var random = data.split(",");
 
-	spotify.search({ type: 'track', query: random[1]}, function(err, data) {
-	    if (err) {
+  spotify.search({ type: 'track', query: random[1]}, function(err, data) {
+      if (err) {
           console.log(err);
         }
         var mySpotify = ("\nArtist: " + data.tracks.items[0].album.artists[0].name + "\nThe Song's Name: " + data.tracks.items[0].name + "\nThe Preview Link: " + data.tracks.items[0].preview_url + "\nThe Album this song is from: " + data.tracks.items[0].album.name + "\n");
-		console.log(mySpotify); 
-     });
-    });
+    console.log(mySpotify); 
+    fs.appendFile("log.txt", mySpotify, function(err, data) {
+         if (err) throw err;
+     }); 
+   });
+ });
 }
